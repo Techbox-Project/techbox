be9c2c1448bfc88d7a1eb57f776d1aea9902d9ae
Checkium <checkium@gmail.com>
2020-12-30 05:22:28 +0000

    ✨ Main configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,12 +13,15 @@ application {
 }
 
 repositories {
+    mavenCentral()
     jcenter()
 }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.21")
     implementation("net.dv8tion:JDA:4.2.0_224")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.12.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.0")
     testImplementation("io.kotest:kotest-runner-junit5:4.3.2")
 }
 
