af4428d4c51bec49bbc745b187bba0b2ef1c63e8
Checkium <checkium@gmail.com>
2021-01-02 00:45:18 +0000

    🎨 Organize dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,21 +17,32 @@ repositories {
 }
 
 dependencies {
+    // Base
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.21")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
     implementation("net.dv8tion:JDA:4.2.0_225")
+
+    // JSON
     implementation("com.fasterxml.jackson.core:jackson-core:2.12.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.12.0")
+
+    // Core
     implementation("com.google.guava:guava:30.1-jre")
     implementation("ch.qos.logback:logback-classic:1.2.3")
     implementation("io.github.classgraph:classgraph:4.8.98")
-    implementation("me.xdrop:fuzzywuzzy:1.3.1")
-    implementation("net.jodah:expiringmap:0.5.9")
+
+    // Database
     implementation("com.zaxxer:HikariCP:3.4.5")
     implementation("org.jetbrains.exposed:exposed-core:0.25.1")
     implementation("org.jetbrains.exposed:exposed-dao:0.25.1")
     implementation("org.jetbrains.exposed:exposed-jdbc:0.25.1")
     implementation("org.postgresql:postgresql:42.2.18")
+
+    // Utils
+    implementation("me.xdrop:fuzzywuzzy:1.3.1")
+    implementation("net.jodah:expiringmap:0.5.9")
+
+    // Test
     testImplementation("io.kotest:kotest-runner-junit5:4.3.2")
 }
 
